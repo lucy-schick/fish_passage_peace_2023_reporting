cd6e738ed6ffc689a101ccbc120713614dd9f5de
almac2022 <al@newgraphenvironment.com>
2024-01-08 10:05:03 -0800

    move to pak as installer and tidy up some

--- scripts/packages.R
+++ scripts/packages.R
@@ -1,7 +1,6 @@
 # # install.packages('pacman')
 
-package_list <- c(
-  'plotKML',  #plot kml needed to go ahead of other packages for some reason and wants to reinstall everytime.... not sure why. hash out for now
+pkgs_cran <- c(
   # 'raster', #load this dog before dplyr yo
   'tidyverse',
   'readwritesqlite',
@@ -13,16 +12,14 @@ package_list <- c(
   'kableExtra',
   'httr',
   'RPostgres',
-  'RPostgreSQL',
+  # 'RPostgreSQL',
   'DBI',
   'magick',
-  # 'bcdata',
+  'bcdata',
   'jpeg',
   'datapasta',
   'knitr',
   'data.table',
-  'lubridate',
-  'forcats',
   'bookdown',
   'fasstr',
   'tidyhydat',
@@ -37,7 +34,10 @@ package_list <- c(
   'xlsx',
   'pagedown',
   'crosstalk',
-  'DT'
+  'DT',
+  'fishbc',
+  'chron',
+  'xfun'
   # 'analogsea',
   # 'here'
   # rgl,
@@ -46,18 +46,26 @@ package_list <- c(
   # gert  ##to track git moves
   ##leafpop I think
 )
+pkgs_gh <- c(#'Envirometrix/plotKML',  #plot kml needed to go ahead of other packages for some reason and wants to reinstall everytime.... not sure why. hash out for nowpoissonconsulting/fwapgr",
+             'poissonconsulting/poisspatial',
+             'poissonconsulting/fwapgr',
+             "newgraphenvironment/fpr"
+             )
 
-# lapply(package_list,
-#        require,
-#        character.only = TRUE)
-
-pacman::p_load(package_list,
-               character.only = TRUE)
+pkgs_all <- c(pkgs_cran,
+              pkgs_gh)
 
 
-pacman::p_load_gh("poissonconsulting/fwapgr",
-                  'poissonconsulting/poisspatial',
-                  "crsh/citr",
-                  'rstudio/pagedown',
-                  "poissonconsulting/fishbc",
-                  "newgraphenvironment/fpr")
+# install or upgrade all the packages with pak
+lapply(pkgs_all,
+       pak::pkg_install)
+
+# load all the packages
+
+pkgs_ld <- c(pkgs_cran,
+             basename(pkgs_gh))
+
+lapply(pkgs_ld,
+       require,
+       character.only = TRUE)
+
