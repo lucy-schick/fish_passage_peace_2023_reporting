3271bf505698ada3b68884dea36fc1b620f7dec2
almac2022 <al@newgraphenvironment.com>
2024-05-26 16:05:15 -0700

    ad news to appendix

--- scripts/functions.R
+++ scripts/functions.R
@@ -140,4 +140,20 @@ tfpr_copy_over_photos <- function(filescopy, filespaste){
 }
 
 
+# write the contents of the NEWS.md file to a RMD file that will be included as an appendix
+news_to_appendix <- function(
+    md_name = "NEWS.md",
+    rmd_name = "2090-report-change-log.Rmd",
+    appendix_title = "# Report Change Log") {
 
+  # Read and modify the contents of the markdown file
+  news_md <- readLines(md_name)
+  news_md <- stringr::str_replace(news_md, "^#", "###") |>
+    stringr::str_replace_all("(^(### .*?$))", "\\1 {-}")
+
+  # Write the title, a blank line, and the modified contents to the Rmd file
+  writeLines(
+    c(paste0(appendix_title, " {-}"), "", news_md),
+    rmd_name
+  )
+}
