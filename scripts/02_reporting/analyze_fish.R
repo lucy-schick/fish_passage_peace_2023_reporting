e3967aabd578241c943360c5c9fa82a8edee1a74
almac2022 <al@newgraphenvironment.com>
2024-05-26 16:06:09 -0700

    updates

--- scripts/02_reporting/analyze_fish.R
+++ scripts/02_reporting/analyze_fish.R
@@ -1,49 +1,13 @@
-##note that this was not run this year - we just used last years to save time
+library(cowplot)
 
 
-source('R/functions.R')
-source('R/packages.R')
-source('R/private_info.R')
-
-##get the road info from the database
-conn <- DBI::dbConnect(
-  RPostgres::Postgres(),
-  dbname = dbname,
-  host = host,
-  port = port,
-  user = user,
-  password = password
-)
-
-##ran this in rstudio sever (cmd) to get the latest
-
-# bcdata bc2pg WHSE_FISH.FISS_FISH_OBSRVTN_PNT_SP
-# should update our whse_fish.fiss_fish_obsrvtn_events_sp table with https://github.com/NewGraphEnvironment/bcfishobs but not going to get into it right now.
+fpr_db_query(fpr_dbq_lscols(table_name = 'streams'))
 
 
-# # ##list tables in a schema
-dbGetQuery(conn,
-           "SELECT table_name
-           FROM information_schema.tables
-           WHERE table_schema='whse_fish'")
-# # # # #
-# # # # # ##list column names in a table
-dbGetQuery(conn,
-           "SELECT column_name,data_type
-           FROM information_schema.columns
-           WHERE table_schema = 'whse_fish'
-           and table_name='fiss_fish_obsrvtn_events_sp'")
-
-dbGetQuery(conn,
-           "SELECT column_name,data_type
-           FROM information_schema.columns
-           WHERE table_name='streams'")
-
-
-
-##get some stats for WCT - we are going to do this from bcbarriers for now since discharge is there. bcfishobs is the local schema
 query = "SELECT
-  fish_observation_point_id,
+  e.fish_observation_point_id,
+  e.species_code,
+  e.geom,
   s.gradient,
   s.stream_order,
   s.upstream_area_ha,
@@ -51,41 +15,103 @@ query = "SELECT
   s.channel_width,
   s.watershed_group_code,
   s.linear_feature_id,
-  s.accessibility_model_wct,
   round((ST_Z((ST_Dump(ST_LocateAlong(s.geom, e.downstream_route_measure))).geom))::numeric) as elevation
-FROM whse_fish.fiss_fish_obsrvtn_events_sp e
+FROM bcfishobs.fiss_fish_obsrvtn_events_vw e
 INNER JOIN bcfishpass.streams s
 ON e.linear_feature_id = s.linear_feature_id
-WHERE e.species_code = 'WCT'
-AND e.downstream_route_measure > s.downstream_route_measure;"
+WHERE e.downstream_route_measure > s.downstream_route_measure;"
+# WHERE e.species_code = 'WCT'
+
 # AND e.watershed_group_code = 'ELKR';" ##looks like only one wct elsewhere anyway which is odd since they show
 # get the correct segmented ID with the downstream route measure
 
 
-wct <- st_read(conn, query = query)
+
+f_raw <- fpr::fpr_db_query(query = query)
+
+
+f_tidy <- f_raw |>
+  # tidy the gradient data so we filter out anything < 0 and > 0.3
+  dplyr::filter(gradient > 0 & gradient < 0.3) |>
+  mutate(gradient_percent = round(gradient*100,0))
+
+# lets have a look at the grayling data
+f_tidy |>
+  dplyr::filter(species_code == "GR") |>
+  esquisse::esquisser()
+
+# ---------- gradient
+sp <- c('GR')
+lab_x <- "Gradient (%)"
+lab_y <- "Count (#)"
+plot_fish_hist <- ggplot(f_tidy |>
+                           dplyr::filter(species_code %in% sp)) +
+  geom_bar(aes(gradient_percent),
+           fill = "#56B4E9", alpha = 0.8) +
+  labs(x = lab_x, y = lab_y) +
+  scale_x_binned(
+    limits = c(0,10),
+    n.breaks = 12) +
+  scale_y_continuous(
+    # don't expand y scale at the lower end
+    expand = expansion(mult = c(0, 0.05))
+  ) +
+  # facet_grid(~species_code) +
+  cowplot::theme_minimal_hgrid() +
+  cowplot::panel_border()
+
+plot_fish_hist
+
+
+# ---------- channel width
+sp <- c('GR')
+lab_x <- "Channel Width (m)"
+lab_y <- "Count (#)"
+
+plot_fish_hist <- ggplot(f_tidy |>
+                           dplyr::filter(species_code %in% sp)) +
+  geom_bar(aes(channel_width),
+           fill = "#56B4E9", alpha = 0.8) +
+  labs(x = lab_x, y = lab_y) +
+  scale_x_binned(
+    limits = c(0,50),
+    n.breaks = 5) +
+  scale_y_continuous(
+    # don't expand y scale at the lower end
+    expand = expansion(mult = c(0, 0.05))
+  ) +
+  # facet_grid(~species_code) +
+  cowplot::theme_minimal_hgrid() +
+  cowplot::panel_border()
+
+plot_fish_hist
+
+
+
+
+
 
 # ggplot(wct, aes(x=channel_width, y=mad_m3s)) +
 #   geom_point()
 
-wct_elkr_grad <- wct %>%
-  mutate(Gradient = case_when(
-    gradient < .03 ~ '0 - 3 %',
-    gradient >= .03 &  gradient < .05 ~ '03 - 5 %',
-    gradient >= .05 &  gradient < .08 ~ '05 - 8 %',
-    gradient >= .08 &  gradient < .15 ~ '08 - 15 %',
-    gradient >= .15 &  gradient < .22 ~ '15 - 22 %',
-    gradient >= .22  ~ '22+ %')) %>%
-  group_by(Gradient)  %>%
-  summarise(Count = n()) %>%
-  mutate(total = nrow(wct),
-         Percent = round(Count/total * 100, 0))
-
-wct_elkr_grad$gradient_id <- c(3,5,8,15,22,99)
+# wct_elkr_grad <- wct %>%
+#   mutate(Gradient = case_when(
+#     gradient < .03 ~ '0 - 3 %',
+#     gradient >= .03 &  gradient < .05 ~ '03 - 5 %',
+#     gradient >= .05 &  gradient < .08 ~ '05 - 8 %',
+#     gradient >= .08 &  gradient < .15 ~ '08 - 15 %',
+#     gradient >= .15 &  gradient < .22 ~ '15 - 22 %',
+#     gradient >= .22  ~ '22+ %')) %>%
+#   group_by(Gradient)  %>%
+#   summarise(Count = n()) %>%
+#   mutate(total = nrow(wct),
+#          Percent = round(Count/total * 100, 0))
+#
+# wct_elkr_grad$gradient_id <- c(3,5,8,15,22,99)
 
 ##save this for the report
 ##burn it all to a file we can use later
 wct_elkr_grad %>% readr::write_csv(file = paste0(getwd(), '/data/inputs_extracted/02_prep_report/wct_mad_chanwidthgrade.csv'))
 
-dbDisconnect(conn = conn)
 
 
